d77dc0e9ef58fbf01c6bd30aae2d2cadc60dc759
MoonshineSpooby <tja411@gmail.com>
2024-09-25 22:34:35 -0700

    procfile update

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ const express = require('express')
 const app = express()
 const PORT = 8000
 const cors = require('cors')
-const DIR_NAME = "./"
-const MAIN_HTML = "main.html"
+
 
 app.use(cors())
 
-app.use(express.static('/public'))
-
+app.use(express.static(__dirname + '/public/'))
+app.get('*', (req, res) => {
+    res.sendFile(__dirname + '/public/main.html')
+})
 app.listen(process.env.PORT || PORT, () => {
     console.log(`express server running on port: ${PORT}`)
 })
