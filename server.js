22ca05f7ae97753d52e8f7eb9aadc3ba6a3a33ce
MoonshineSpooby <tja411@gmail.com>
2024-09-25 21:46:30 -0700

    adding stylesheet and script

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const app = express()
 const PORT = 8000
+const cors = require('cors')
 const DIR_NAME = "./"
 const MAIN_HTML = "main.html"
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/main.html')
-    // response.sendFile(`${DIR_NAME}${MAIN_HTML}`)
-})
+
+app.use(cors())
+
+app.use(express.static('public'))
 
 app.listen(process.env.PORT || PORT, () => {
     console.log(`express server running on port: ${PORT}`)
